e3f5b57bc3cf612c5a87f37341e0d5553a8c47d2
mahnyb <mahnybarazandehtar1998@gmail.com>
2024-05-04 21:46:48 +0300

    updates

--- Sorting_Ceremony.py
+++ Sorting_Ceremony.py
@@ -10,12 +10,12 @@ def sortingHat():
     print("\nWelcome to the Sorting Ceremony where we will determine which Hogwarts House you belong to... ")
 
     # Question 1
-    print("\nQ1) Do you like Dawn or Dusk?")
+    print("\nQ1) Do you like Dawn or Dusk?\n")
     print("1) Dawn")
     print("2) Dusk")
 
     while True:
-        answer = input("Your answer(1/2): ")
+        answer = input("\nYour answer(1/2): ")
 
         if answer == '1':
             GryffindorPoints += 2
@@ -26,17 +26,17 @@ def sortingHat():
             HufflePuffPoints += 2
             break
         else:
-            print("Invalid Input. Please Enter either 1 or 2.")
+            print("\nInvalid Input. Please Enter either 1 or 2.")
             
     
 
     # Question 2
-    print("\nQ2) Do you like Forest or River?")
+    print("\nQ2) Do you like Forest or River?\n")
     print("1) Forest")
     print("2) River")
 
     while True:
-        answer = input("Your answer(1/2): ")
+        answer = input("\nYour answer(1/2): ")
 
         if answer == '1':
             GryffindorPoints += 2
@@ -47,16 +47,16 @@ def sortingHat():
             HufflePuffPoints += 2
             break
         else:
-            print("Invalid Input. Please Enter either 1 or 2.")
+            print("\nInvalid Input. Please Enter either 1 or 2.")
         
     
     # Question 3
-    print("\nQ3) Do you like Moon or Stars?")
+    print("\nQ3) Do you like Moon or Stars?\n")
     print("1) Moon")
     print("2) Stars")
 
     while True:
-        answer = input("Your answer(1/2): ")
+        answer = input("\nYour answer(1/2): ")
 
         if answer == '1':
             SlytherinPoints += 2
@@ -67,18 +67,18 @@ def sortingHat():
             GryffindorPoints += 2
             break
         else:
-            print("Invalid Input. Please Enter either 1 or 2.")
+            print("\nInvalid Input. Please Enter either 1 or 2.")
         
 
     # Question 4
-    print("\nQ4) Which of the following would you most hate people to call you?")
+    print("\nQ4) Which of the following would you most hate people to call you?\n")
     print("1) Ordinary")
     print("2) Ignorant")
     print("3) Cowardly")
     print("4) Selfish")
 
     while True:
-        answer = input("Your answer(1/2/3/4): ")
+        answer = input("\nYour answer(1/2/3/4): ")
 
         if answer == '1':
             SlytherinPoints += 2
@@ -93,17 +93,17 @@ def sortingHat():
             HufflePuffPoints += 2
             break
         else:
-            print("Invalid Input. Please choose one of the valid options")
+            print("\nInvalid Input. Please choose one of the valid options")
 
 
     # Question 5
-    print("\nQ5) After you have died, what would you most like people to do when they hear your name?")
+    print("\nQ5) After you have died, what would you most like people to do when they hear your name?\n")
     print("1) Miss you, but smile.")
     print("2) Ask for more stories about your adventures")
     print("3) Think with admiration of your achievements")
     print("4) I don't care what people think of me after I'm dead, it's what they think of me while I'm alive that counts")
 
-    answer = input("Your answer(1/2/3/4): ")
+    answer = input("\nYour answer(1/2/3/4): ")
 
     if answer == '1':
         HufflePuffPoints += 2
@@ -114,18 +114,18 @@ def sortingHat():
     elif answer == '4':
         SlytherinPoints += 2
     else:
-        print("Wrong Input")
+        print("\nInvalid Input. Please choose one of the valid options")
         return
 
     # Question 6
-    print("\nQ6) How would you like to be known to history?")
+    print("\nQ6) How would you like to be known to history?\n")
     print("1) The Wise")
     print("2) The Good")
     print("3) The Great")
     print("4) The Bold")
 
     while True: 
-        answer = input("Your answer(1/2/3/4): ")
+        answer = input("\nYour answer(1/2/3/4): ")
 
         if answer == '1':
             RavenclawPoints += 2
@@ -140,18 +140,18 @@ def sortingHat():
             GryffindorPoints += 2
             break
         else:
-            print("Invalid Input. Please choose one of the valid options")
+            print("\nInvalid Input. Please choose one of the valid options")
         
 
     # Question 7
-    print("\nQ7) Given the choice, would you rather invent a potion that would guarantee you:")
+    print("\nQ7) Given the choice, would you rather invent a potion that would guarantee you:\n")
     print("1) Love?")
     print("2) Glory?")
     print("3) Wisdom?")
     print("4) Power?")
 
     while True:
-        answer = input("Your answer(1/2/3/4): ")
+        answer = input("\nYour answer(1/2/3/4): ")
 
         if answer == '1':
             HufflePuffPoints += 2
@@ -166,18 +166,18 @@ def sortingHat():
             SlytherinPoints += 2
             break
         else:
-            print("Invalid Input. Please choose one of the valid options")
+            print("\nInvalid Input. Please choose one of the valid options")
         
     
     # Question 8
-    print("\nQ8) Once every century, the Flutterby bush produces flowers that adapt their scent to attract the unwary. \n If it lured you, it would smell of:")
+    print("\nQ8) Once every century, the Flutterby bush produces flowers that adapt their scent to attract the unwary. \n If it lured you, it would smell of:\n")
     print("1) A crackling log fire")
     print("2) The Sea")
     print("3) Fresh Parchment")
     print("4) Home")
 
     while True: 
-        answer = input("Your answer(1/2/3/4): ")
+        answer = input("\nYour answer(1/2/3/4): ")
 
         if answer == '1':
             GryffindorPoints += 2
@@ -192,18 +192,18 @@ def sortingHat():
             HufflePuffPoints += 2
             break
         else:
-            print("Invalid Input. Please choose one of the valid options")
+            print("\nInvalid Input. Please choose one of the valid options")
 
     
     # Question 9
-    print("\nQ9) Four goblets are placed before you. Which would you choose to drink?")
+    print("\nQ9) Four goblets are placed before you. Which would you choose to drink?\n")
     print("1) The foaming, frothing, silvery liquid that sparkles as though containing ground diamonds.")
     print("2) The smooth, thick, richly purple drink that gives off a delicious smell of chocolate and plums.")
     print("3) The golden liquid so bright that it hurts the eye, and which makes sunspots dance all around the room. ")
     print("4) The mysterious black liquid that gleams like ink, and gives off fumes that make you see strange visions.")
 
     while True:
-        answer = input("Your answer(1/2/3/4): ")
+        answer = input("\nYour answer(1/2/3/4): ")
 
         if answer == '1':
             RavenclawPoints += 2
@@ -218,10 +218,17 @@ def sortingHat():
             SlytherinPoints += 2
             break
         else:
-            print("Invalid Input. Please choose one of the valid options")
+            print("\nInvalid Input. Please choose one of the valid options")
 
+    house_points = {
+        'Gryffindor': GryffindorPoints,
+        'Slytherin': SlytherinPoints,
+        'Ravenclaw': RavenclawPoints,
+        'Hufflepuff': HufflePuffPoints
+    }
 
-
+    maxPoints = max(house_points, key=house_points.get)
+    print(f"\nCongratulations! You belong to {maxPoints}!!")
 
 if __name__ == "__main__":
     sortingHat()
